7345840645016a17126a86995f18c5c94db7838d
melamela <272866@student.pwr.edu.pl>
2024-08-26 14:56:58 +0200

    Widok dnia dla zawodnika

--- front_end/src/Player/DayView/DayView.js
+++ front_end/src/Player/DayView/DayView.js
@@ -1,4 +1,29 @@
 import styles from "./DayView.module.css";
+
+// Tu sie domyślam, że można by utworzyć jeden komponent
+// i jakoś sparametryzować kolor, żeby nie powtarzać kodu,
+// ale na razie chcę po prostu zrobić cokolwiek
+
+const Activity = ({title, wykonany, odczucia, komentarz, color, borderColor}) => {
+    return (
+    <div className={styles.activity} 
+        style={{
+            backgroundColor: color,
+            borderColor: borderColor
+            }}
+    >
+            <div>
+                <h3>{title}</h3>
+                <p>Wykonany: {wykonany}</p>
+                <p>Odczucia: {odczucia}</p>
+                <p>Komentarz: {komentarz}</p>
+            </div>
+            {/* <div> tu bedzie strzałka w prawo </div> */}
+        </div>
+    );
+}
+
+
 const DayView = () => {
     return (
         <div className={styles.background}>
@@ -19,10 +44,6 @@ const DayView = () => {
                     Piotr Kopiec
                 </div>
                 
-
-
-
-                
                 {/* <div onClick={toggleSidebar}>
                     <RxHamburgerMenu className={styles.burger}/>
                 </div>
@@ -35,8 +56,63 @@ const DayView = () => {
                     </div>
                 </div>   */}
             </div>
-            <div>
-                smierdzisz
+
+            <div className = {styles.layout}>
+
+                    <div className = {styles.rectangleStats}> {/* Statystyki dnia */}
+                        <p className = {styles.dayHeader}>STATYSTYKI DNIA</p> {/*tu sobie sprawdzę headery*/}
+                        <div className = {styles.text}>
+                            <p> Tętno: 100 </p>
+                            <p> Samopoczucie: złe </p>
+                            <p> Zakwaszenie: TAK/NIE(przycisk!!!) </p>
+                            <p> Kinaza: idk co to jest</p>
+                        </div>
+                    </div>
+               
+                    <div className = {styles.rectangleSActivities}>  {/*  Aktywności */}
+                    <p className = {styles.dayHeader}>  AKTYWNOŚCI </p>
+                        <div>
+                           <Activity
+                                title="Trening motoryczny"
+                                wykonany="tak"
+                                odczucia="Zle"
+                                komentarz="brak"
+                                color="#FF7A68"
+                                borderColor="#BE0000"
+                               
+                           
+                           />
+                            <Activity
+                                title="Trening biegowy"
+                                wykonany="tak"
+                                odczucia="Zle"
+                                komentarz="brak"
+                                color="#95C6FF"
+                                borderColor="#0056BA"
+                               
+                           />
+                            <Activity
+                                title="Trening mata"
+                                wykonany="tak"
+                                odczucia="Zle"
+                                komentarz="brak"
+                                color="#FFF281"
+                                borderColor="#CCB700"
+                           />
+                            <Activity
+                                title="Fizjoterapia"
+                                wykonany="tak"
+                                odczucia="Zle"
+                                komentarz="brak"
+                                color="#83FF8F"
+                                borderColor="#00C514"
+                           />    
+
+                        </div>
+                        
+
+                    </div>
+
             </div>
             
         </div>
