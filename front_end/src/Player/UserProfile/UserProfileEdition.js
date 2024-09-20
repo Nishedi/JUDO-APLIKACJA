e868b34c655294ace055580553816cde80ff1baa
melamela <272866@student.pwr.edu.pl>
2024-09-20 23:47:13 +0200

    Poprawa w edycji profilu zawodnika, bez zapisywania do bazy

--- front_end/src/Player/UserProfile/UserProfileEdition.js
+++ front_end/src/Player/UserProfile/UserProfileEdition.js
@@ -1,136 +1,146 @@
 import styles from './UserProfileEdition.module.css';
-import React from 'react';
-import { LiaAccessibleIcon } from 'react-icons/lia';
+import React, { useState, useContext } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GlobalContext } from '../../GlobalContext';
-import { useContext } from 'react';
-
-
 
 const UserProfileEdition = () => {
-// Zmienne stanu przechowujące wartości pól formularza'
-   const {globalVariable, setGlobalVariable, supabase} = useContext(GlobalContext);
+   const { globalVariable, setGlobalVariable, supabase } = useContext(GlobalContext);
    const [password, setPassword] = useState('');
    const [confirmPassword, setConfirmPassword] = useState('');
    const [error, setError] = useState('');
    const navigate = useNavigate();
 
-// Dynamiczne uzyskanie bieżącego roku
    const currentYear = new Date().getFullYear();
    const oldestYear = currentYear - 100;
-
-// Generowanie opcji lat od bieżącego roku do 100 lat wstecz
    const years = [];
+
+
+   
    for (let i = currentYear; i >= oldestYear; i--) {
        years.push(i);
    }
-   
-// Funkcja obsługująca wysłanie formularza
-   const handleSubmit = (e) => {
-   e.preventDefault(); // Zapobiega domyślnej akcji wysłania formularza
 
-   if (password !== confirmPassword) {
-       setError('Hasła nie są takie same');
-       return;
-   }
+   // Funkcja obsługująca wysłanie formularza
+   const handleSubmit = async (e) => {
+       e.preventDefault(); // Zapobiega domyślnej akcji wysłania formularza
 
-   // Jeśli hasła są takie same, wyślij formularz
-   setError('');
-   alert('Formularz został wysłany');
+       if (password !== confirmPassword) {
+           setError('Hasła nie są takie same');
+           return;
+       }
 
-   // Zaktualizowanie danych użytkownika
-   setGlobalVariable({
-      ...globalVariable,
-      firstName: e.target.firstname.value,
-      lastName: e.target.lastname.value,
-      login: e.target.login.value,
-      gender: e.target.gender.value,
-      birthYear: e.target.year.value,
-      weightCategory: e.target.weight.value
-  });
+       setError('');
 
-   // Zmiana flagi na zakończenie edycji
-   navigate('/player/userprofile');
-}
+       const { firstname, lastname, login, gender, year, weight } = e.target;
 
+       try {
+           // Zaktualizowanie danych użytkownika w Supabase
+           const { data, error } = await supabase
+               .from('users')
+               .update({
+                   imie: firstname.value,
+                   nazwisko: lastname.value,
+                   login: login.value,
+                   plec: gender.value,
+                   rocznik: year.value,
+                   kategoria_wagowa: weight.value,
+                   haslo: password // Pamiętaj o bezpiecznym przechowywaniu haseł
+               })
+               .eq('id', globalVariable.id); // Użyj ID użytkownika do aktualizacji
 
-       return (
-         <div className = {styles.background}>
-            {/* Navbar */}
-            <div className={styles.navbar}>
-                  <div className={styles.burger}>
-                     <RxHamburgerMenu/>
-                  </div>
-                  <div className={styles.profilDiv}>
-                        <div className={styles.end} >
-                           Edycja danych 
-                        </div>
-                        
-                  </div>
-            </div>
-            {/* Body */}
-            <div className={styles.userdetails}>
+            if (error) {
+               console.error('Błąd podczas aktualizacji danych: UPDATE', error);
+               throw error;
+            }
+
+           // Zaktualizowanie stanu globalnego
+           setGlobalVariable({
+               ...globalVariable,
+               imie: firstname.value,
+               nazwisko: lastname.value,
+               login: login.value,
+               plec: gender.value,
+               rocznik: year.value,
+               kategoria_wagowa: weight.value,
+           });
+
+           // Zmiana flagi na zakończenie edycji
+           navigate('/player/userprofile');
+       } catch (error) {
+           console.error('Błąd podczas aktualizacji danych:UP2', error);
+           setError('Nie udało się zaktualizować danych. Spróbuj ponownie.');
+       }
+   };
+
+   return (
+       <div className={styles.background}>
+           {/* Navbar */}
+           <div className={styles.navbar}>
+               <div className={styles.burger}>
+                   <RxHamburgerMenu />
+               </div>
+               <div className={styles.profilDiv}>
+                   <div className={styles.end}>Edycja danych</div>
+               </div>
+           </div>
+           {/* Body */}
+           <div className={styles.userdetails}>
                <div>
-                  <h2>Dane użytkownika</h2>
+                   <h2>Dane użytkownika</h2>
                </div>
                <div>
-                  <form onSubmit={handleSubmit}>
-                     <label>Imię:</label>
-                     <input type="text" name="firstname" />
+                   <form onSubmit={handleSubmit}>
+                       <label>Imię:</label>
+                       <input type="text" name="firstname" defaultValue={globalVariable.imie} required />
 
-                     <label>Nazwisko:</label>
-                     <input type="text" name="lastname" />
+                       <label>Nazwisko:</label>
+                       <input type="text" name="lastname" defaultValue={globalVariable.nazwisko} required />
 
-                     <label>Płeć</label>
-                     <select name="gender">
-                        <option value="Mężczyzna">Mężczyzna</option>
-                        <option value="Kobieta">Kobieta</option>
-                     </select>
+                       <label>Płeć:</label>
+                       <select name="gender" defaultValue={globalVariable.plec} required>
+                           <option value="Mężczyzna">Mężczyzna</option>
+                           <option value="Kobieta">Kobieta</option>
+                       </select>
 
-                     <label>Rocznik:</label>
-                     <select name="year">
-                        {years.map(year =>
-                           <option 
-                              key={year} value={year}>{year}
-                           </option>
-                        )}
-                        </select>
+                       <label>Rocznik:</label>
+                       <select name="year" defaultValue={globalVariable.rocznik} required>
+                           {years.map(year => (
+                               <option key={year} value={year}>{year}</option>
+                           ))}
+                       </select>
 
-                     <label>Kategoria wagowa:</label>
-                     <input type="text" name="weight" />
+                       <label>Kategoria wagowa:</label>
+                       <input type="text" name="weight" defaultValue={globalVariable.kategoria_wagowa} required />
 
-                     <label>Login:</label>
-                     <input type="text" name="login" />
+                       <label>Login:</label>
+                       <input type="text" name="login" defaultValue={globalVariable.login} required />
 
-                     <label>Hasło:</label>
-                     <input 
-                        type="password" 
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                     />
+                       <label>Hasło:</label>
+                       <input 
+                           type="password" 
+                           name="password"
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                       />
 
-                     <label>Potwierdź hasło:</label>
-                     <input 
+                       <label>Potwierdź hasło:</label>
+                       <input 
                            type="password" 
                            name="confirmPassword" 
                            value={confirmPassword} 
                            onChange={(e) => setConfirmPassword(e.target.value)} 
-                     />
+                       />
 
-                     {error && <p style={{ color: 'red' }}>{error}</p>}
-
-
-                     <div className={styles.buttoncenter}>
-                        <button className={styles.buttonEdit} type="submit">Zapisz</button>
-                     </div>
-                  </form>
+                       {error && <p style={{ color: 'red' }}>{error}</p>}
+                       <div className={styles.buttoncenter}>
+                           <button className={styles.buttonEdit} type="submit">Zapisz</button>
+                       </div>
+                   </form>
                </div>
-            </div>
+           </div>
+       </div>
+   );
+};
 
-         </div>
-       );
-   }
-   export default UserProfileEdition;
+export default UserProfileEdition;
