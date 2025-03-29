47a74fe5e43c60043f8882d4bb4833cd03297638
NISHEDI <263948@student.pwr.edu.pl>
2025-03-29 18:04:39 +0100

    klikacze na dni do smsow

--- front_end/src/GlobalContext.js
+++ front_end/src/GlobalContext.js
@@ -8,6 +8,7 @@ export const GlobalProvider = ({ children }) => {
   const [newPlayer, setNewPlayer] = useState(null);
   const [viewedPlayer, setViewedPlayer] = useState(null);
   const [sms, setSms] = useState(null);
+  const [smsList, setSmsList] = useState([]);
 
   const supabaseUrl = 'https://akxozdmzzqcviqoejhfj.supabase.co';
   const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFreG96ZG16enFjdmlxb2VqaGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyNTA3NDYsImV4cCI6MjAzOTgyNjc0Nn0.FoI4uG4VI_okBCTgfgIPIsJHWxB6I6ylOjJEm40qEb4";
@@ -19,8 +20,10 @@ export const GlobalProvider = ({ children }) => {
     const storedNewPlayer = localStorage.getItem('newPlayer');
     const storedViewedPlayer = localStorage.getItem('viewedPlayer');
     const storedSms = localStorage.getItem('sms');
+    const storedSmsList = localStorage.getItem('smsList');
     if (storedGlobalVariable) setGlobalVariable(JSON.parse(storedGlobalVariable));
     if (storedNewPlayer) setNewPlayer(storedNewPlayer);
+    if (storedSmsList) setSmsList(JSON.parse(storedSmsList));
     if (storedViewedPlayer) {
       const player = JSON.parse(storedViewedPlayer);
       if(player.currentDate){
@@ -56,6 +59,11 @@ export const GlobalProvider = ({ children }) => {
     }
   }, [viewedPlayer]);
   useEffect(() => {
+    if (smsList !== null) {
+      localStorage.setItem('smsList', JSON.stringify(smsList));
+    }
+  }, [smsList]);
+  useEffect(() => {
     if (sms !== null) {
       localStorage.setItem('sms', JSON.stringify(sms));
     }
@@ -70,7 +78,8 @@ export const GlobalProvider = ({ children }) => {
       newPlayer, setNewPlayer,
       viewedPlayer, setViewedPlayer,
       supabase,
-      sms, setSms
+      sms, setSms,
+      smsList, setSmsList
     }}>
       {children}
     </GlobalContext.Provider>
